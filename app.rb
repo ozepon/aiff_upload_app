efa7fd4158deaef8c69a8a34662dc1d6a23b88eb
ozepon <dandy.buzz@gmail.com>
2016-08-29 02:59:14 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,38 @@
+class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  logger = Logger.new('app.log')
+
+  # root
+  get '/' do
+    slim :index
+    # 'hellossss'
+  end
+
+  # file upload
+  post '/upload' do
+    # tempfile削除
+    # tempfile.close(true)
+
+    aiff_path = "./aiff/"
+    FileUtils.mkdir_p(aiff_path) unless FileTest.exist?(aiff_path)
+
+    upload_file = "#{aiff_path}#{params[:file][:filename]}"
+
+    begin
+      File.open(upload_file, 'w') do |f|
+        f.write params[:file][:tempfile].read
+      end
+
+      # ここにshellを指定する
+      result = `echo 'hello'`
+      logger.info "create #{upload_file}"
+    rescue => e
+      logger.warn e.message
+      halt 500, '失敗っす'
+    end
+    'upload 成功'
+  end
+end
---
+++ config.ru
@@ -0,0 +1,8 @@
+# Gemfileのライブラリ読み込み
+Bundler.require
+
+# アプリのルーティング読み込み
+require './app.rb'
+
+# sinatra起動
+run App
